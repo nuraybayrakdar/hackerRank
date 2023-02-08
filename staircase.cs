b3cd774d30b4229e483bf2b4967eba5d5bbfc58a
nuraybayrakdar <bayrakdarnuray@gmail.com>
2023-02-09 01:18:16 +0300

    staircase

---
+++ staircase.cs
@@ -0,0 +1,41 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n)
+    {
+        for(int i = 0; i<n; i++){
+            Console.WriteLine(new string(' ', n - i - 1) + new string('#', i + 1));
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
